7cd97425e73f4b07f909ddba1f473cd562080ef7
TheMeinerLP <p.glanz@madfix.me>
2023-05-01 19:15:11 +0200

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [require("tw-elements/dist/plugin.cjs")]
+
+}
+
