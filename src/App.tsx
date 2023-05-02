86d7a5fc40aeb8b52f602c52b96c98433bd0a7d2
TheMeinerLP <p.glanz@madfix.me>
2023-05-03 00:08:02 +0200

    Add custom language

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {getZipData} from "./loader/ZipLoader";
 import {Tab, initTE} from "tw-elements";
 import {FileType} from "./models/FileType";
 import hljs from 'highlight.js';
+import {latestlogHighlightjs} from "./utils/latestlog-highlightjs";
 
 
 function createTabContent(fileContent: DebugFileContent, index: number) {
@@ -28,7 +29,7 @@ function createTabContent(fileContent: DebugFileContent, index: number) {
             case FileType.JSON:
                 return "language-json";
             case FileType.LOG:
-                return "language-accesslog"
+                return "language-latestlog"
             case FileType.YAML:
                 return "language-yaml"
         }
@@ -109,8 +110,9 @@ function App() {
     useEffect(() => {
         initTE({Tab});
         document.querySelectorAll("pre code").forEach(
-            (el) =>  hljs.highlightElement(el as HTMLElement)
+            (el) => hljs.highlightElement(el as HTMLElement)
         );
+        hljs.registerLanguage("latestlog", latestlogHighlightjs);
     });
     const params = useParams();
     const [value, setValue] = useState<DebugFileContent[]>([])
@@ -125,12 +127,25 @@ function App() {
     const tabs = value.map((value1, index) => createTabEntry(value1.uiTabName, index));
     const contents = value.map((value1, index) => createTabContent(value1, index));
     return (<div className={"dark bg-slate-800"}>
-        <ul
-            className="flex list-none flex-row flex-wrap"
-            role="tablist"
-            data-te-nav-ref>
-            {tabs}
-        </ul>
+        <nav
+            className="flex-no-wrap relative flex w-full items-center justify-between bg-neutral-100 py-0 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-0" data-te-navbar-ref>
+            <div>
+                <span className="text-xl font-semibold text-neutral-800 dark:text-neutral-200 mx-2">Debug Paster Viewer</span>
+            </div>
+            <ul className="flex list-none flex-row flex-wrap mr-auto lg:flex-row" role="tablist" data-te-nav-ref>
+                {tabs}
+            </ul>
+            <div className="relative flex items-center">
+                <a className="mr-4 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                   href="#">
+                    <span className="[&>svg]:w-5">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                           className="h-5 w-5"><path
+                          d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"/></svg>
+                    </span>
+                </a>
+            </div>
+        </nav>
         <div>
             {contents}
         </div>
---
+++ src/utils/latestlog-highlightjs.ts
@@ -0,0 +1,32 @@
+import {HLJSApi, Language} from "highlight.js";
+
+export function latestlogHighlightjs(hljs: HLJSApi): Language {
+    return {
+        name: "Paper latest log",
+        contains: [
+            {
+                end: "(\\]: )",
+                contains: [
+                    // Time
+                    {
+                        match: "(\\[[0-9]{2}:[0-9]{2}:[0-9]{2}\\])",
+                        scope: "regexp",
+                        relevance: 1
+                    },
+                    // Level
+                    {
+                        match: "(\\[[A-Za-z #0-9]*\\/([A-Z]*)\\])",
+                        scope: "meta",
+                        relevance: 0
+                    },
+                    {
+                        begin: "(\\w*)?",
+                        scope: "string",
+                        relevance: 0
+                    }
+                ]
+            }
+
+        ]
+    };
+}
