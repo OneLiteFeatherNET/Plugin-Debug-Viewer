806dc5ad43859ba7e73984adacb7b5bf7185713e
TheMeinerLP <p.glanz@madfix.me>
2023-05-03 00:39:09 +0200

    Fix security issue

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ function createTabEntry(name: String, index: number) {
     const arialTarget = `tabs-${index}`
     if (index === 0) {
         return (
-            <li role="presentation" key={index}>
+            <li role="presentation" key={index} className={"flex-grow basis-0 text-center"}>
                 <a
                     href={target}
                     className="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
@@ -142,7 +142,7 @@ function App() {
             </ul>
             <div className="relative flex items-center">
                 <a className="mr-4 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                   href="https://github.com/OneLiteFeatherNET/Plugin-Debug-Viewer" target={"_blank"}>
+                   href="https://github.com/OneLiteFeatherNET/Plugin-Debug-Viewer" target={"_blank"} rel={"noreferrer"}>
                     <FontAwesomeIcon icon={["fab", "github"]} size={"xl"} />
                 </a>
             </div>
